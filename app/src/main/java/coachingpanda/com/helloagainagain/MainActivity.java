13c1c48b04b40d9f7e62da43a079260a01f69980
vinodkgupta96 <vinodkgupta.1996@gmail.com>
2018-02-03 09:14:35 +0530

    Test added variable a

--- app/src/main/java/coachingpanda/com/helloagainagain/MainActivity.java
+++ app/src/main/java/coachingpanda/com/helloagainagain/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int a=10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        a=a+1;
         setContentView(R.layout.activity_main);
     }
 }
